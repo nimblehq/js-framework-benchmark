f4050b103222a514e2c2022399cb2adc39c18b93
Olivier Robert <olivierobert@gmail.com>
2022-09-27 11:28:35 +0700

    [#2] Add remaining unit tests for the user repository

--- nextjs/repositories/user.repository.test.ts
+++ nextjs/repositories/user.repository.test.ts
@@ -3,6 +3,52 @@ import { dbClientMock } from '../tests/database'
 import { userFactory } from '../tests/factories/user.factory'
 
 describe('User Respository', () => {
+  describe('findUserById', () => {
+    describe('given there is a matching user', () => {
+      it('returns a user', async () => {
+        const userAttributes = {
+          id: '1'
+        }
+        const user = {...userFactory, ...userAttributes};
+      
+        dbClientMock.user.findUnique.mockResolvedValue(user);
+  
+        await expect(findUserById('1')).resolves.toEqual(user)
+      });
+    });
+
+    describe('given there is NO matching user', () => {
+      it('return null', async () => {      
+        dbClientMock.user.findUnique.mockResolvedValue(null);
+  
+        await expect(findUserById('1')).resolves.toEqual(null)
+      });
+    });
+  });
+
+  describe('findUserByEmail', () => {
+    describe('given there is a matching user', () => {
+      it('returns a user', async () => {
+        const userAttributes = {
+          email: 'dev@nimblehq.co'
+        }
+        const user = {...userFactory, ...userAttributes};
+      
+        dbClientMock.user.findUnique.mockResolvedValue(user);
+  
+        await expect(findUserByEmail('dev@nimblehq.co')).resolves.toEqual(user)
+      });
+    });
+
+    describe('given there is NO matching user', () => {
+      it('returns null', async () => {      
+        dbClientMock.user.findUnique.mockResolvedValue(null);
+  
+        await expect(findUserByEmail('dev@nimblehq.co')).resolves.toEqual(null)
+      });
+    });
+  });
+
   describe('createUser', () => {
     it('creates a user', async () => {
       const userAttributes = {
