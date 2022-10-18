3f23f1a4f347324fad2a8b50ca082a967d368f4c
Olivier Robert <olivierobert@gmail.com>
2022-10-18 11:09:11 +0700

    [#3] Add suffix `.page` to all pages

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx'],
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src', 'stylesheets')],
