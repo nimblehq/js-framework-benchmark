7b071dada95d0868e01d77494ef4e1be715c654e
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-05-23 15:40:51 +0700

    [#38] Add nextauth signin callback

--- nextjs-13/src/config/auth.ts
+++ nextjs-13/src/config/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
+import { createUser, findUserByEmail } from "repositories/user.repository";
 
 export const authOptions = {
   providers: [
@@ -8,7 +9,38 @@ export const authOptions = {
       clientSecret: process.env.OAUTH_GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ profile }: any) {
+      try {
+        const { picture, name, email } = profile;
+
+        if (email === undefined) {
+          return false
+        }
+
+        const existingUser = await findUserByEmail(
+          email
+        );
+
+        if (existingUser) {
+          return true
+        }
+
+        const userAttributes = {
+          avatarUrl: picture,
+          name: name,
+          email: email,
+        };
+
+        await createUser(userAttributes);
+
+        return true
+      } catch (error) {
+        return false
+      }
+    }
+  }
 }
 
 export default NextAuth(authOptions)
