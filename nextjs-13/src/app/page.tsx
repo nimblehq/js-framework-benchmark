479a7ce4bda681006be2f6f79f91ced6145bac76
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-05-23 10:22:24 +0700

    [#38] Add login button to homescreen

--- nextjs-13/src/app/page.tsx
+++ nextjs-13/src/app/page.tsx
@@ -1,5 +1,32 @@
-const Home = () => {
-  return <div>JS Framework Benchmark</div>
-}
+// import type { ReactElement } from 'react';
 
-export default Home
+import Head from 'next/head';
+import Link from 'next/link';
+
+// import AuthLayout from 'components/AuthLayout';
+// import type { AppNextPage } from 'pages/_app';
+
+const AuthSignInPage = () => {
+// const AuthSignInPage: AppNextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Sign in | NextNewsletter 🚀</title>
+      </Head>
+      <h4>NextNewsletter 🚀</h4>
+      <Link href="../api/auth/sign-in">
+        <button className="btn" data-testid="loginButton">
+          Login with Google
+        </button>
+      </Link>
+    </>
+  );
+};
+
+// AuthSignInPage.authRequired = false;
+
+// AuthSignInPage.setLayout = (page: ReactElement) => {
+//   return <AuthLayout>{page}</AuthLayout>;
+// };
+
+export default AuthSignInPage;
