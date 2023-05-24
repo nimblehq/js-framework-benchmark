445d8b030e5135bef6dbfb31cd46bb8a71c372e0
Phisith.L (Beckham) <59072804+phisith@users.noreply.github.com>
2023-05-24 13:33:00 +0700

    sessionStoreage define variable in ENV

--- remix/app/services/session.server.ts
+++ remix/app/services/session.server.ts
@@ -2,12 +2,12 @@ import { createCookieSessionStorage } from "@remix-run/node";
 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
-    name: "__session",
-    sameSite: "lax",
-    path: "/",
-    httpOnly: true,
-    secrets: ["veryscret"],
-    secure: true,
+    name: process.env.SESSION_NAME,
+    sameSite: process.env.SESSION_SAMESITE,
+    path: process.env.SESSION_PATH,
+    httpOnly: process.env.SESSION_HTTPONLY ? true : false,
+    secrets: process.env.SESSION_SECRETS,
+    secure: process.env.SESSION_SECURE ? true : false,
   },
 });
 
