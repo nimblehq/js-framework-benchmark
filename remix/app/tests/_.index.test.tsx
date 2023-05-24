e105fdf3a60d6311742e92ddceaa3b9ad3972964
Phisith.L (Beckham) <59072804+phisith@users.noreply.github.com>
2023-05-24 09:38:48 +0700

    Fix new request

--- remix/app/tests/_.index.test.tsx
+++ remix/app/tests/_.index.test.tsx
@@ -2,7 +2,7 @@ import Index from "../routes/_index";
 import { render } from "@testing-library/react";
 
 describe("Test Root route", () => {
-  test("Should render Welcome Page", () => {
+  test("renders Welcome Page", () => {
     const { getByText } = render(<Index />);
     expect(getByText("Welcome to Remix")).toBeInTheDocument();
   });
