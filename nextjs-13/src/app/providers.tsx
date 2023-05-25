cb90e3ad6d81a3941a1f77868f07a73822a8c64c
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-05-25 10:45:55 +0700

    [#39] Add styles for header and user profile components

--- nextjs-13/src/app/providers.tsx
+++ nextjs-13/src/app/providers.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import App from "./app";
 
 type Props = {
   children?: React.ReactNode;
 };
 
 export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+  <SessionProvider>
+    <App>
+      {children}
+    </App>
+  </SessionProvider>
+  )
 };
