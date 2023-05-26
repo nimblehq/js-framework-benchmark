cfb32b5933586ec49d2df15c7945895f642b3daf
Phisith.L (Beckham) <59072804+phisith@users.noreply.github.com>
2023-05-26 09:41:45 +0700

    [#57] Update database schema and Refactor tests with faker.js

--- remix/app/repositories/user.server.test.ts
+++ remix/app/repositories/user.server.test.ts
@@ -1,19 +1,14 @@
 import type { User } from '@prisma/client';
 
 import UserRespository from '../repositories/user.server';
+import { userFactory } from '../tests/factories/user.factory';
 import { prismaMock } from '../tests/setup-test-env';
 
 describe('User Repository', () => {
   describe('updateOrCreate', () => {
-    describe('given there is a user information', async () => {
+    describe('given there is a user information', () => {
       it('creates a user', async () => {
-        const user: User = {
-          name: 'Beckham',
-          email: 'phisith@nimblehq.co',
-          avatarUrl: 'weofkwepofkwefokwepfokwpo',
-          createdAt: new Date(),
-          id: 1,
-        };
+        const user = { ...userFactory };
 
         prismaMock.user.upsert.mockResolvedValue(user);
 
@@ -25,22 +20,19 @@ describe('User Repository', () => {
   });
 
   describe('findBy', () => {
-    describe('given there is a user information', async () => {
+    describe('given there is a user information', () => {
       it('returns a user', async () => {
-        const user: User = {
-          name: 'Beckham',
-          email: 'phisith@nimblehq.co',
-          avatarUrl: 'weofkwepofkwefokwepfokwpo',
-          createdAt: new Date(),
-          id: 1,
-        };
-        const param = {
+        const userAttributes = {
           email: 'phisith@nimblehq.co',
         } as User;
 
+        const user = { ...userFactory, ...userAttributes };
+
         prismaMock.user.findUnique.mockResolvedValue(user);
 
-        await expect(UserRespository.findBy(param)).resolves.toEqual(user);
+        await expect(UserRespository.findBy(userAttributes)).resolves.toEqual(
+          user
+        );
       });
     });
   });
