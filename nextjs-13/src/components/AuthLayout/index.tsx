2365fc2f9f99e7c5e53ecce43222a664006b7eda
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-05-26 13:43:05 +0700

    [#39] Improve css of sign in page and app header

---
+++ nextjs-13/src/components/AuthLayout/index.tsx
@@ -0,0 +1,20 @@
+import Card from 'components/Card';
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <div
+      className='layout-auth'
+      data-testid="authLayout"
+    >
+      <main className="app-content">
+        <Card>{children}</Card>
+      </main>
+    </div>
+  );
+};
+
+export default AuthLayout;
