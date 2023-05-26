0ae60ab98b9170872718cadd71e108ead2213560
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-05-26 14:50:33 +0700

    [#39] Add UI test for page sign-in

---
+++ nextjs-13/src/app/auth/sign-in/page.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useSession, signIn } from 'next-auth/react';
+import { redirect } from 'next/navigation';
+import SignInPage from './page';
+
+jest.mock('next-auth/react');
+jest.mock('next/navigation');
+
+describe('SignInPage', () => {
+  describe('Session status is "loading', () => {
+    it('renders loading state', () => {
+      useSession.mockReturnValue({ status: 'loading' });
+      render(<SignInPage />);
+      expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
+  });
+  describe('Session status is "authenticated', () => {
+    it.only('redirects to home page', () => {
+      useSession.mockReturnValue({ status: 'authenticated' });
+      render(<SignInPage />);
+      expect(redirect).toHaveBeenCalledWith('/');
+    });
+  });
+  describe('Session status is "unauthenticated', () => {
+    it('renders sign in button', () => {
+      useSession.mockReturnValue({ status: 'unauthenticated' });
+      render(<SignInPage />);
+      expect(screen.getByTestId('loginButton')).toBeInTheDocument();
+    });
+    it('calls signIn on button click', async () => {
+      signIn
+      useSession.mockReturnValue({ status: 'unauthenticated' });
+      render(<SignInPage />);
+      // console.log('========>screen.getByTestId("loginButton") : ', userEvent.click(screen.getByTestId('loginButton')))
+      await userEvent.click(screen.getByTestId('loginButton'));
+      expect(signIn).toHaveBeenCalledWith('google');
+    });
+  });
+});
