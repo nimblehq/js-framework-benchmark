3fbc86ca07e3c631017d99ea591e93f79ba79186
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-05-30 09:10:07 +0700

    [#44] Use baseHandler for fetchMe API

--- nextjs-13/src/app/api/v1/me/route.ts
+++ nextjs-13/src/app/api/v1/me/route.ts
@@ -1,28 +1,11 @@
 import { User } from '@prisma/client';
+import baseHandler from 'lib/handler/base.handler';
 import { NextResponse, NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-
-import * as UserRepository from '../../../../repositories/user.repository';
 
 export type ApiMeResponse = { user: User };
 
 export async function GET(req: NextRequest) {
-  try {
-    const token = await getToken({ req });
-
-    if (token && token.userId) {
-      const currentUser = await UserRepository.findUserById(
-        token.userId as string
-      );
-
-      return NextResponse.json({ user: currentUser }, { status: 200 });
-    }
-
-    return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
-  } catch (error) {
-    return NextResponse.json(
-      { message: (error as Error).message },
-      { status: 500 }
-    );
-  }
+  return baseHandler(req, async (currentUser) => {
+    return NextResponse.json({ user: currentUser }, { status: 200 });
+  })
 }
