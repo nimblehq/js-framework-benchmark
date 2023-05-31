4064e682bd54ad998c0b13572e644cf440dad8a3
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-05-31 13:45:37 +0700

    [#45] Improve eslint

--- nextjs-13/src/app/api/v1/me/route.ts
+++ nextjs-13/src/app/api/v1/me/route.ts
@@ -1,11 +1,12 @@
 import { User } from '@prisma/client';
-import baseHandler from 'lib/handler/base.handler';
 import { NextResponse, NextRequest } from 'next/server';
+
+import baseHandler from 'lib/handler/base.handler';
 
 export type ApiMeResponse = { user: User };
 
 export async function GET(req: NextRequest) {
   return baseHandler(req, async (currentUser) => {
     return NextResponse.json({ user: currentUser }, { status: 200 });
-  })
+  });
 }
