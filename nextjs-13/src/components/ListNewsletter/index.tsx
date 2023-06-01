952f0f5ed8dee8ff841aaf0b9db1a6bcedf40ee9
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-01 12:55:55 +0700

    [#43] Add ListNewsletter

---
+++ nextjs-13/src/components/ListNewsletter/index.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+import requestManager from 'lib/request/manager';
+import promiseWrapper from 'lib/request/promiseWrapper';
+
+const ListNewsletter = () => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      const promise = requestManager('GET', 'v1/newsletter').then(
+        (res) => res.records
+      );
+      setRecords(promiseWrapper(promise));
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div className="list-newsletter">
+      <div>
+        <ul className="">
+          {records.map((item) => {
+            return (
+              <li className="" key={item.id}>
+                <span className="">{item.name}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ListNewsletter;
---
+++ nextjs-13/src/lib/request/promiseWrapper.tsx
@@ -0,0 +1,30 @@
+const promiseWrapper = (promise) => {
+  let status = 'pending';
+  let result;
+
+  const s = promise.then(
+    (value) => {
+      status = 'success';
+      result = value;
+    },
+    (error) => {
+      status = 'error';
+      result = error;
+    }
+  );
+
+  return () => {
+    switch (status) {
+      case 'pending':
+        throw s;
+      case 'success':
+        return result;
+      case 'error':
+        throw result;
+      default:
+        throw new Error('Unknown status');
+    }
+  };
+};
+
+export default promiseWrapper;
