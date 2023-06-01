e6a3fb1de38081c27a19c5eae776ec6d93475ca4
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-01 09:45:14 +0700

    [#42] Add queryNewsletterByUserId and tests

--- nextjs-13/src/repositories/newsletter.repository.test.ts
+++ nextjs-13/src/repositories/newsletter.repository.test.ts
@@ -1,7 +1,10 @@
 import { dbClientMock } from '@test/database';
 import { newsletterFactory } from '@test/factories/newsletter.factory';
 
-import { createNewsletter } from './newsletter.repository';
+import {
+  createNewsletter,
+  queryNewsletterByUserId,
+} from './newsletter.repository';
 
 describe('Newsletter Respository', () => {
   describe('createNewsletter', () => {
@@ -18,6 +21,32 @@ describe('Newsletter Respository', () => {
 
       await expect(createNewsletter(newsletterAttributes)).resolves.toEqual(
         newsletter
+      );
+    });
+  });
+
+  describe('queryNewsletterByUserId', () => {
+    it('returns newsletters', async () => {
+      const userId = '1';
+
+      const newsletterAttributes = {
+        name: 'iOS 17 release',
+        content:
+          'iOS 17 will reportedly have a new feature that turns locked iPhones into smart home displays. The interface will display information such as appointments, the weather, and notifications. It will operate similarly to smart home devices from other companies.',
+        userId: userId,
+      };
+      const newsletter = { ...newsletterFactory, ...newsletterAttributes };
+      const newsletters = [newsletter, newsletter];
+
+      const user = {
+        id: userId,
+        newsletters,
+      };
+
+      dbClientMock.user.findUnique.mockResolvedValue(user);
+
+      await expect(queryNewsletterByUserId(userId)).resolves.toEqual(
+        newsletters
       );
     });
   });
--- nextjs-13/src/repositories/newsletter.repository.ts
+++ nextjs-13/src/repositories/newsletter.repository.ts
@@ -4,4 +4,13 @@ import { Newsletter } from '../models/newsletter.model';
 const createNewsletter = async (newsletterAttributes: Newsletter) =>
   dbClient.newsletter.create({ data: newsletterAttributes });
 
-export { createNewsletter };
+const queryNewsletterByUserId = async (id: string) => {
+  const user = await dbClient.user.findUnique({
+    where: { id: id },
+    include: { newsletters: true },
+  });
+
+  return user?.newsletters;
+};
+
+export { createNewsletter, queryNewsletterByUserId };
