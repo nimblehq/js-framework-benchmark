1107fbeea704ecac1a4f9d96390380ad260f36da
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-02 11:32:57 +0700

    [#48] Add API to delete newsletter

--- nextjs-13/src/repositories/newsletter.repository.ts
+++ nextjs-13/src/repositories/newsletter.repository.ts
@@ -6,13 +6,8 @@ const createNewsletter = async (
   newsletterAttributes: Prisma.NewsletterCreateInput
 ) => dbClient.newsletter.create({ data: newsletterAttributes });
 
-const deleteNewsletter = async ({
-  id,
-  userId,
-}: {
-  id: string;
-  userId: string;
-}) => dbClient.newsletter.deleteMany({ where: { id: id, userId: userId } });
+const deleteNewsletter = async (id: string, userId: string) =>
+  dbClient.newsletter.deleteMany({ where: { id: id, userId: userId } });
 
 const queryNewsletterByUserId = async (id: string) => {
   const user = await dbClient.user.findUnique({
