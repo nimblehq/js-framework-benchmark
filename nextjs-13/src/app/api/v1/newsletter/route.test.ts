38db813356c28dedbe7965970a26a722d8edaa45
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-05 12:20:07 +0700

    [#42] Add tests for 'GET /v1/newsletter'

--- nextjs-13/src/app/api/v1/newsletter/route.test.ts
+++ nextjs-13/src/app/api/v1/newsletter/route.test.ts
@@ -4,13 +4,17 @@
 import { Newsletter, Prisma } from '@prisma/client';
 import { StatusCodes } from 'http-status-codes';
 
-import { dbClientMock } from '@test/database';
 import { newsletterFactory } from '@test/factories/newsletter.factory';
 import appHandler from 'lib/handler/app.handler';
+import {
+  createNewsletter,
+  queryNewsletterByUserId,
+} from 'repositories/newsletter.repository';
 
-import { POST } from './route';
+import { GET, POST } from './route';
 
 jest.mock('lib/handler/app.handler');
+jest.mock('repositories/newsletter.repository');
 
 describe('POST /v1/newsletter', () => {
   afterEach(() => {
@@ -34,11 +38,17 @@ describe('POST /v1/newsletter', () => {
       appHandler.mockImplementation((req, callback) =>
         callback(user, requestBody)
       );
-      dbClientMock.newsletter.create.mockResolvedValue(newsletter);
+
+      createNewsletter.mockResolvedValue(newsletter);
 
       const res = await POST(requestBody);
       const responseBody = await res.json();
 
+      expect(createNewsletter).toHaveBeenCalledWith({
+        name: newsletter.name,
+        content: newsletter.content,
+        user: { connect: { id: user.id } },
+      });
       expect(res.status).toBe(StatusCodes.OK);
       expect(responseBody.newsletter).toMatchObject<Newsletter>({
         id: newsletterAttributes.id,
@@ -54,11 +64,12 @@ describe('POST /v1/newsletter', () => {
   describe('given invalid params', () => {
     it('return invalid data error', async () => {
       const user = { id: '1' };
+      const content = newsletterFactory.content;
       const requestBody = {
         json: () => {
           return {
             name: null,
-            content: newsletterFactory.content,
+            content: content,
           };
         },
       };
@@ -66,17 +77,46 @@ describe('POST /v1/newsletter', () => {
       appHandler.mockImplementation((req, callback) =>
         callback(user, requestBody)
       );
-      dbClientMock.newsletter.create.mockImplementation(() => {
+      createNewsletter.mockImplementation(() => {
         throw new Prisma.PrismaClientValidationError();
       });
 
       const res = await POST(requestBody);
       const responseBody = await res.json();
 
+      expect(createNewsletter).toHaveBeenCalledWith({
+        name: null,
+        content: content,
+        user: { connect: { id: user.id } },
+      });
       expect(res.status).toBe(StatusCodes.UNPROCESSABLE_ENTITY);
       expect(responseBody).toMatchObject({
         message: 'Invalid params',
       });
+    });
+  });
+});
+
+describe('GET /v1/newsletter', () => {
+  it.only('returns a list of newsletters', async () => {
+    const user = { id: '1' };
+    const newsletterAttributes = { id: '1', userId: user.id };
+    const newsletter = { ...newsletterFactory, ...newsletterAttributes };
+    appHandler.mockImplementation((req, callback) => callback(user, {}));
+    queryNewsletterByUserId.mockResolvedValue([newsletter]);
+
+    const res = await GET({});
+    const responseBody = await res.json();
+
+    expect(queryNewsletterByUserId).toHaveBeenCalledWith(user.id);
+    expect(res.status).toBe(StatusCodes.OK);
+    expect(responseBody.records[0]).toMatchObject<Newsletter>({
+      id: newsletterAttributes.id,
+      name: expect.any(String),
+      content: expect.any(String),
+      userId: newsletterAttributes.userId,
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
     });
   });
 });
