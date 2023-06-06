fd05611ef658113ba2c26288ab0ea63ec34d24d9
Phisith.L (Beckham) <59072804+phisith@users.noreply.github.com>
2023-06-06 18:52:52 +0700

    [#64] Add button for navigate to create newsletter page

---
+++ remix/app/components/AppLink/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from '@remix-run/react';
+
+interface ButtonProps {
+  name: string;
+  href: string;
+}
+
+export default function AppLink({ name, href }: ButtonProps) {
+  return (
+    <>
+      <Link
+        to={href}
+        className="text-center px-4 py-2 font-semibold text-white bg-slate-900 hover:bg-slate-700 shadow rounded-lg"
+      >
+        {name}
+      </Link>
+    </>
+  );
+}
--- remix/app/routes/_app._index.tsx
+++ remix/app/routes/_app._index.tsx
@@ -1,12 +1,19 @@
 import { User } from '@prisma/client';
 import { useOutletContext } from '@remix-run/react';
 
+import AppLink from '../components/AppLink';
+
 export default function Index() {
   const [user] = useOutletContext() as [User];
 
   return (
     <>
-      <h1>JS Framework Benchmark | {user?.name}</h1>
+      <section>
+        <div className="flex justify-end">
+          <AppLink href="/newsletter/create" name={'Create Newsletter'} />
+        </div>
+        <h1>JS Framework Benchmark | {user?.name}</h1>
+      </section>
     </>
   );
 }
