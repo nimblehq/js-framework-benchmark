7fd3bced27f8827b4ffe62cc398cbc8295d7a095
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-07 18:05:56 +0700

    [#51] Modify layout to prepare for new Send Newsletter Page

--- nextjs-13/src/app/layout.tsx
+++ nextjs-13/src/app/layout.tsx
@@ -1,3 +1,8 @@
+'use client';
+import Head from 'next/head';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import App from './app';
 import { NextAuthProvider } from './session-providers';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,12 +13,41 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const classNames = require('classnames');
+  const isRoot = !pathname.includes('/send');
+
   return (
     <html lang="en">
       <body>
         <ToastContainerWrapper />
         <NextAuthProvider>
-          <App>{children}</App>
+          <App>
+            <div className="dashboard">
+              <nav className="dashboard__nav">
+                <Link
+                  className={classNames('dashboard__tab', {
+                    'dashboard__tab--selected': isRoot,
+                  })}
+                  href="/"
+                >
+                  Manage Newsletter
+                </Link>
+                {/* <div className="dashboard__tab dashboard__tab--selected">
+                Manage Newsletter
+              </div> */}
+                <Link
+                  href="/send"
+                  className={classNames('dashboard__tab', {
+                    'dashboard__tab--selected': !isRoot,
+                  })}
+                >
+                  Send Newsletter
+                </Link>
+              </nav>
+              {children}
+            </div>
+          </App>
         </NextAuthProvider>
       </body>
     </html>
