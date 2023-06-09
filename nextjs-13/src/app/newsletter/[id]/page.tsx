fc72626229da0900110546ff73144716b0f1c5f5
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-09 11:38:29 +0700

    [#53] Add `ViewNewsletter` page and `NewsletterDetail` component

---
+++ nextjs-13/src/app/newsletter/[id]/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Suspense, useEffect, useState } from 'react';
+import { ClipLoader } from 'react-spinners';
+
+import { useParams, redirect } from 'next/navigation';
+
+import NewsletterDetail from '@components/NewsletterDetail';
+import requestManager from 'lib/request/manager';
+import promiseWrapper from 'lib/request/promiseWrapper';
+
+const ViewNewsletter = () => {
+  const [promise, setPromise] = useState();
+
+  const params = useParams();
+  const id = params.id;
+
+  async function fetchRecords() {
+    try {
+      const res = await requestManager('GET', `v1/newsletter/${id}`);
+
+      return res.record;
+    } catch (error) {
+      redirect('/auth/sign-in');
+    }
+  }
+
+  useEffect(() => {
+    setPromise(promiseWrapper(fetchRecords()));
+  }, []);
+
+  return (
+    <div className="view-newsletter">
+      <Suspense fallback={<ClipLoader loading={true} size={150} />}>
+        <NewsletterDetail promise={promise} />
+      </Suspense>
+    </div>
+  );
+};
+
+export default ViewNewsletter;
---
+++ nextjs-13/src/components/NewsletterDetail/index.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+const NewsletterDetail = ({ promise }) => {
+  const newsletter = promise ? promise.read() : {};
+
+  return (
+    <div className="newsletter">
+      <h3 className="name">{newsletter.name}</h3>
+      <p className="content">{newsletter.content}</p>
+      <br />
+    </div>
+  );
+};
+
+export default NewsletterDetail;
