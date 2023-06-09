bf9efa63f231a3143fe11990da523fdf32f1b9bc
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-09 17:56:36 +0700

    Delete APIs that are replaced by Server Actions

--- nextjs-13/src/app/api/v1/newsletter/[id]/route.test.ts
+++ nextjs-13/src/app/api/v1/newsletter/[id]/route.test.ts
@@ -4,115 +4,12 @@
 import { StatusCodes } from 'http-status-codes';
 
 import { newsletterFactory } from '@test/factories/newsletter.factory';
-import { userFactory } from '@test/factories/user.factory';
-import appHandler from 'lib/handler/app.handler';
-import {
-  deleteNewsletter,
-  findNewsletter,
-  updateNewsletter,
-} from 'repositories/newsletter.repository';
+import { findNewsletter } from 'repositories/newsletter.repository';
 
-import { DELETE, PUT, GET } from './route';
+import { GET } from './route';
 
 jest.mock('lib/handler/app.handler');
 jest.mock('repositories/newsletter.repository');
-
-describe('DELETE /v1/newsletter/:id', () => {
-  let user: { id: string };
-  let newsletter: { id: string };
-
-  beforeEach(() => {
-    user = { id: '1' };
-    newsletter = { id: '2' };
-
-    appHandler.mockImplementation((_, callback) => callback(user, {}));
-    deleteNewsletter.mockResolvedValue({ count: 1 });
-  });
-
-  describe('newsletter exists', () => {
-    it('deletes the newsletter', async () => {
-      const response = await DELETE({}, { params: { id: newsletter.id } });
-
-      expect(deleteNewsletter).toHaveBeenCalledWith(newsletter.id, user.id);
-      expect(response.status).toBe(StatusCodes.OK);
-    });
-  });
-
-  describe('newsletter does NOT exist', () => {
-    beforeEach(() => {
-      deleteNewsletter.mockResolvedValue({ count: 0 });
-    });
-
-    it('returns error', async () => {
-      const response = await DELETE({}, { params: { id: newsletter.id } });
-      const responseBody = await response.json();
-
-      expect(deleteNewsletter).toHaveBeenCalledWith(newsletter.id, user.id);
-      expect(response.status).toBe(StatusCodes.UNPROCESSABLE_ENTITY);
-      expect(responseBody).toMatchObject({
-        message: 'Newsletter could not be deleted',
-      });
-    });
-  });
-});
-
-describe('PUT /v1/newsletter/:id', () => {
-  describe('newsletter exists', () => {
-    it('updates the newsletter', async () => {
-      const user = { ...userFactory, id: '1' };
-      const newsletter = { id: '2' };
-      const data = {
-        name: 'New name',
-        content: 'New content',
-      };
-      const where = {
-        id: newsletter.id,
-        user: user,
-      };
-
-      appHandler.mockImplementation((_, callback) => callback(user, {}));
-      updateNewsletter.mockResolvedValue({ count: 1 });
-
-      const response = await PUT(
-        { json: () => data },
-        { params: { id: newsletter.id } }
-      );
-
-      expect(updateNewsletter).toHaveBeenCalledWith({ where, data });
-      expect(response.status).toBe(StatusCodes.OK);
-    });
-  });
-
-  describe('newsletter does NOT exist', () => {
-    it('returns error', async () => {
-      const user = { ...userFactory, id: '1' };
-      const newsletter = { id: '2' };
-      const data = {
-        name: 'New name',
-        content: 'New content',
-      };
-      const where = {
-        id: newsletter.id,
-        user: user,
-      };
-
-      appHandler.mockImplementation((_, callback) => callback(user, {}));
-      updateNewsletter.mockResolvedValue({ count: 0 });
-
-      const response = await PUT(
-        { json: () => data },
-        { params: { id: newsletter.id } }
-      );
-      const responseBody = await response.json();
-
-      expect(updateNewsletter).toHaveBeenCalledWith({ where, data });
-      expect(response.status).toBe(StatusCodes.UNPROCESSABLE_ENTITY);
-      expect(responseBody).toMatchObject({
-        message: 'Newsletter could not be updated',
-      });
-    });
-  });
-});
 
 describe('GET /v1/newsletter/:id', () => {
   describe('newsletter exists', () => {
@@ -120,11 +17,11 @@ describe('GET /v1/newsletter/:id', () => {
       const newsletter = newsletterFactory;
       findNewsletter.mockResolvedValue(newsletter);
 
-      const res = await GET({}, { params: { id: newsletter.id } });
-      const responseBody = await res.json();
+      const response = await GET({}, { params: { id: newsletter.id } });
+      const responseBody = await response.json();
 
       expect(findNewsletter).toHaveBeenCalledWith(newsletter.id);
-      expect(res.status).toBe(StatusCodes.OK);
+      expect(response.status).toBe(StatusCodes.OK);
       expect(responseBody.record).toMatchObject({
         id: newsletter.id,
         name: expect.any(String),
@@ -139,11 +36,11 @@ describe('GET /v1/newsletter/:id', () => {
     it('returns error', async () => {
       findNewsletter.mockResolvedValue(null);
 
-      const res = await GET({}, { params: { id: '1' } });
-      const responseBody = await res.json();
+      const response = await GET({}, { params: { id: '1' } });
+      const responseBody = await response.json();
 
       expect(findNewsletter).toHaveBeenCalledWith('1');
-      expect(res.status).toBe(StatusCodes.UNPROCESSABLE_ENTITY);
+      expect(response.status).toBe(StatusCodes.UNPROCESSABLE_ENTITY);
       expect(responseBody).toMatchObject({
         message: 'Newsletter could not be viewed',
       });
--- nextjs-13/src/app/api/v1/newsletter/[id]/route.ts
+++ nextjs-13/src/app/api/v1/newsletter/[id]/route.ts
@@ -1,75 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
 import { NextResponse, NextRequest } from 'next/server';
 
-import appHandler from 'lib/handler/app.handler';
 import { invalidParamsResponseError } from 'lib/request/error';
-import {
-  deleteNewsletter,
-  findNewsletter,
-  updateNewsletter,
-} from 'repositories/newsletter.repository';
-
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  return appHandler(req, async (currentUser) => {
-    try {
-      const result = await deleteNewsletter(params.id, currentUser.id);
-
-      if (result.count === 0) {
-        return NextResponse.json(
-          { message: 'Newsletter could not be deleted' },
-          { status: StatusCodes.UNPROCESSABLE_ENTITY }
-        );
-      }
-
-      return NextResponse.json(
-        { status: 'success' },
-        { status: StatusCodes.OK }
-      );
-    } catch (err) {
-      return invalidParamsResponseError();
-    }
-  });
-}
-
-export async function PUT(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  return appHandler(req, async (currentUser) => {
-    try {
-      const { name, content } = await req.json();
-
-      const data = { name, content };
-
-      const where = {
-        id: params.id,
-        user: currentUser,
-      };
-
-      const result = await updateNewsletter({
-        where,
-        data,
-      });
-
-      if (!result.count) {
-        return NextResponse.json(
-          { message: 'Newsletter could not be updated' },
-          { status: StatusCodes.UNPROCESSABLE_ENTITY }
-        );
-      }
-
-      return NextResponse.json(
-        { status: 'success' },
-        { status: StatusCodes.OK }
-      );
-    } catch (err) {
-      return invalidParamsResponseError();
-    }
-  });
-}
+import { findNewsletter } from 'repositories/newsletter.repository';
 
 export async function GET(
   _: NextRequest,
@@ -86,6 +19,6 @@ export async function GET(
 
     return NextResponse.json({ record }, { status: StatusCodes.OK });
   } catch (err) {
-    return getInvalidParamsError();
+    return invalidParamsResponseError();
   }
 }
