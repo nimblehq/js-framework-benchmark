33ad873ccd503620e17903caecb620e190b251aa
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-09 11:36:52 +0700

    [#53] Add new Public Layout

---
+++ nextjs-13/src/components/PublicLayout/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { status } = useSession();
+
+  return (
+    <div className="public" data-testid="public-newsletter">
+      {status === 'unauthenticated' && (
+        <div className="banner">
+          <Link href="auth/sign-in">Sign up now</Link> to create your own
+          newsletters
+        </div>
+      )}
+      <main className="app-content">{children}</main>
+    </div>
+  );
+}
