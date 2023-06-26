64559b097e6d1a108ad3691bd16bb66eeb47a5ca
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-06-26 10:12:54 +0700

    Add comment to the `getServerSession` workaround

--- nextjs-13/src/lib/request/getServerSession.ts
+++ nextjs-13/src/lib/request/getServerSession.ts
@@ -3,6 +3,8 @@ import { getServerSession as originalGetServerSession } from 'next-auth';
 
 import { authOptions } from 'config/auth';
 
+// this is a workaround so that we can use `getServerSession` with Next.js Server Actions
+// source: https://github.com/nextauthjs/next-auth/issues/7486#issuecomment-1543747325
 export const getServerSession = async () => {
   const req = {
     headers: Object.fromEntries(headers() as Headers),
